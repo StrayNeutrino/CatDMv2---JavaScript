505fc2b0c8324957382bb559c32e6aa93ad37740
Christina <strayelectron@gmail.com>
2026-01-21 16:12:55 -0500

    D20 Row Updates
    D20 dropdown now goes to 5.

--- buttonScript.js
+++ buttonScript.js
@@ -1,4 +1,24 @@
-function myFunction() {
- let die = Math.floor(Math.random() * (20 - 1 + 1) + 1);
- document.getElementById("text").innerHTML=die;
+
+function d20Function() {
+    let num = document.getElementById("d20").value;
+    
+    if (num >1) {
+        var result2 = "";
+        for (let i=0; i<num-1; i++) {
+            let die = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+            result2 = result2 + die + ", ";
+        }
+        let die2 = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        let result = result2 + die2;
+        document.getElementById("d20Result").innerHTML=result;
+    } else {        
+        let die = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        document.getElementById("d20Result").innerHTML=die;
+    }
+
+}
+
+function refresh() {
+    document.getElementById("d20Result").innerHTML = "";
+    document.getElementById("d20").selectedIndex = 0;
 }
